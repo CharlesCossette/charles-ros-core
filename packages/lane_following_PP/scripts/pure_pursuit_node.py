902fbbbf1a825f5ee7471c63fa148df2ae7b5234
CharlesCossette <charles.cossette@mail.mcgill.ca>
2019-10-14 14:12:34 -0400

    Pure pursuit node changes

--- packages/lane_following_PP/scripts/pure_pursuit_node.py
+++ packages/lane_following_PP/scripts/pure_pursuit_node.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
-from duckietown_msgs.msg import Twist2DStamped, LanePose, WheelsCmdStamped, BoolStamped, FSMState, StopLineReading
+# Import the different message structures that we will use.
+from duckietown_msgs.msg import SegmentList, Twist2DStamped, LanePose, WheelsCmdStamped, BoolStamped
 import time
 import numpy as np
 
@@ -12,6 +13,74 @@ class purePursuit(object):
         # Publications
         self.pub_car_cmd = rospy.Publisher("~car_cmd", Twist2DStamped, queue_size=1)
         self.pub_radius_limit = rospy.Publisher("~radius_limit", BoolStamped, queue_size=1)
+
+        # Subscriptions
+       # self.sub_lane_reading = rospy.Subscriber("~lane_pose", LanePose, self.PoseHandling, "lane_filter", queue_size=1) # Need to build callback. Might not be necessary
+        self.sub_wheels_cmd_executed = rospy.Subscriber("~wheels_cmd_executed", WheelsCmdStamped, self.updateWheelsCmdExecuted, queue_size=1) # Need to build callback
+        self.sub_actuator_limits = rospy.Subscriber("~actuator_limits", Twist2DStamped, self.updateActuatorLimits, queue_size=1) # Need to build callback
+        self.pub_seglist_filtered = rospy.Subscriber("~seglist_filtered",SegmentList, self.updateCommands, queue_size=1)# Need to build callback
+
+        # safe shutdown
+        rospy.on_shutdown(self.custom_shutdown)
+
+
+    # ##########################################################################
+    # ######################### MAIN CONTROL FUNCTION ######################## #
+    # ##########################################################################
+    def updateCommands(self, segListFilteredMsg)
+        v = 0.5
+        omega = 0
+
+
+        # Publish the command
+        car_control_msg = Twist2DStamped()
+        car_control_msg.v = v
+        car_control_msg.omega = omega
+        self.pub_car_cmd.publish(car_control_msg)
+
+
+
+
+
+
+
+
+    # ########################## HOUSEKEEPING FUNCTIONS ##############################
+    def updateWheelsCmdExecuted(self, msg_wheels_cmd):
+        self.wheels_cmd_executed = msg_wheels_cmd
+
+    def updateActuatorLimits(self, msg_actuator_limits):
+        self.actuator_limits = msg_actuator_limits
+        rospy.logdebug("actuator limits updated to: ")
+        rospy.logdebug("actuator_limits.v: " + str(self.actuator_limits.v))
+        rospy.logdebug("actuator_limits.omega: " + str(self.actuator_limits.omega))
+        msg_actuator_limits_received = BoolStamped()
+        msg_actuator_limits_received.data = True
+        self.pub_actuator_limits_received.publish(msg_actuator_limits_received)
+
+
+    # ############################# CUSTOM SHUTDOWN ##################################
+    def custom_shutdown(self):
+        rospy.loginfo("[%s] Shutting down..." % self.node_name)
+
+        # Stop listening
+        self.sub_lane_reading.unregister()
+        self.sub_obstacle_avoidance_pose.unregister()
+        self.sub_obstacle_detected.unregister()
+        self.sub_intersection_navigation_pose.unregister()
+        self.sub_wheels_cmd_executed.unregister()
+        self.sub_actuator_limits.unregister()
+        self.sub_switch.unregister()
+        self.sub_fsm_mode.unregister()
+
+        # Send stop command
+        car_control_msg = Twist2DStamped()
+        car_control_msg.v = 0.0
+        car_control_msg.omega = 0.0
+        self.publishCmd(car_control_msg)
+
+        rospy.sleep(0.5)    #To make sure that it gets published.
+        rospy.loginfo("[%s] Shutdown" %self.node_name)
 
 
 if __name__ == '__main__':
