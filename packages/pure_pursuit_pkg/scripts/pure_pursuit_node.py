1ed0424ee592d40f5cd39c821d9bf5c511aeeaae
CharlesCossette <charles.cossette@mail.mcgill.ca>
2019-10-14 19:27:38 -0400

    All wired up, just tuning PP algorithm

--- packages/pure_pursuit_pkg/scripts/pure_pursuit_node.py
+++ packages/pure_pursuit_pkg/scripts/pure_pursuit_node.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
+import math
 import rospy
 # Import the different message structures that we will use.
 from duckietown_msgs.msg import Segment, SegmentList, Twist2DStamped, LanePose, WheelsCmdStamped, BoolStamped
 import time
 import numpy as np
 
-class purePursuit(object):
+class pure_pursuit(object):
     # ##########################################################################
     # ############################## CONSTRUCTOR ############################# #
     # ##########################################################################
@@ -22,8 +23,7 @@ class purePursuit(object):
        # self.sub_lane_reading = rospy.Subscriber("~lane_pose", LanePose, self.PoseHandling, "lane_filter", queue_size=1) # Need to build callback. Might not be necessary
         self.sub_wheels_cmd_executed = rospy.Subscriber("~wheels_cmd_executed", WheelsCmdStamped, self.updateWheelsCmdExecuted, queue_size=1) # Need to build callback
         self.sub_actuator_limits = rospy.Subscriber("~actuator_limits", Twist2DStamped, self.updateActuatorLimits, queue_size=1) # Need to build callback
-        self.pub_seglist_filtered = rospy.Subscriber("~seglist_filtered",SegmentList, self.updateCommands, queue_size=1)# Need to build callback
-
+        self.pub_seglist_filtered = rospy.Subscriber("~seglist_filtered",SegmentList, self.updateCommands, queue_size=1)
         # safe shutdown
         rospy.on_shutdown(self.custom_shutdown)
 
@@ -31,16 +31,39 @@ class purePursuit(object):
     # ##########################################################################
     # ######################### MAIN CONTROL FUNCTION ######################## #
     # ##########################################################################
-    def updateCommands(self, segListFilteredMsg):
-        v = 0.5
+    def updateCommands(self, segListMsg):
+        v_bar = 0.3
         omega = 0
+        L = 0.1 # Look-ahead distance
+        L_tol = 0.05 # tolerance on segments which are in lookahead distance.
 
+        segAvgx = 0
+        segAvgy = 0
+        segnum = 0
+        for segment in segListMsg.segments:
+            segx = (segment.points[0].x + segment.points[1].x)/2
+            segy = (segment.points[0].y + segment.points[1].y)/2
+            segdist = math.sqrt(segx ** 2 + segy ** 2)
+            if (segment.color == segment.YELLOW): # Then this segment is near our look-ahead distance.
+                segAvgx = segAvgx + segx
+                segAvgy = segAvgy + segy
+                segnum = segnum + 1
+        
+        if segnum != 0:
+            segAvgx = segAvgx/segnum
+            segAvgy = segAvgy/segnum
+            L = math.sqrt(segAvgx ** 2 + segAvgy ** 2)
+            sinAlpha = segAvgy
+            omega = 1.5*2*v_bar*sinAlpha/L
+        else:
+            omega = 0
 
         # Publish the command
         car_control_msg = Twist2DStamped()
-        car_control_msg.v = v
+        car_control_msg.v = v_bar
         car_control_msg.omega = omega
         self.pub_car_cmd.publish(car_control_msg)
+        print("Angular Velocity: ", omega,"  L: ", L)
 
 
 
@@ -84,7 +107,7 @@ class purePursuit(object):
 
 if __name__ == '__main__':
     rospy.init_node('pure_pursuit', anonymous=False)
-    pure_pursuit_node = purePursuit()
+    pure_pursuit_node = pure_pursuit()
     # if a safe shutdown is necessary
     #rospy.on_shutdown(lane_filter_node.onShutdown) 
     rospy.spin()  
